9f4038ee0c97a9a8cd673325da895d9c4103c36e
AlonsoFavero <alonso.favero@gmail.com>
2026-09-06 12:47:51 -0300

    Implementa equals para comparação lógica de Produto

--- src/Sets/TesteHashSet.java
+++ src/Sets/TesteHashSet.java
@@ -1,11 +1,31 @@
 package Sets;
 
+import collections.Listas.Produto;
+
 import java.util.HashSet;
 
 public class TesteHashSet {
     public static void main(String[] args){
 
         HashSet<String> nomes = new HashSet<>();
+        HashSet<Produto> produtos = new HashSet<>();
+
+        Produto produto1 = new Produto();
+
+        produto1.nome = "notebook";
+        produto1.preco = 1400;
+
+
+        Produto produto2 = new Produto();
+
+        produto2.nome = "notebook";
+        produto2.preco = 1400;
+
+        produtos.add(produto1);
+        produtos.add(produto2);
+
+        System.out.println(produtos);
+        System.out.println(produto1.equals(produto2));
 
         nomes.add("João");
         nomes.add("Maria");
--- src/collections/Listas/Produto.java
+++ src/collections/Listas/Produto.java
@@ -2,11 +2,24 @@ package collections.Listas;
 
 public class Produto {
 
-    String nome;
-    double preco;
+   public String nome;
+   public double preco;
 
     @Override
     public String toString() {
         return nome + " - R$" + preco ;
+    }
+
+
+    @Override
+    public boolean equals(Object obj){
+
+        if (!(obj instanceof Produto)) {
+            return false;
+        }
+
+        Produto outroProduto = (Produto) obj;
+
+        return nome.equals(outroProduto.nome) && preco == outroProduto.preco;
     }
 }
