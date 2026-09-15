95239a75e13c8251deea3b2d3fda348efc2783aa
AlonsoFavero <alonso.favero@gmail.com>
2026-09-15 18:04:02 -0300

    feat: adiciona soma do estoque com Map

---
+++ src/collections/mapas/TesteSomarEstoque.java
@@ -0,0 +1,27 @@
+package collections.mapas;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TesteSomarEstoque {
+
+    public static void main(String[] args) {
+
+        Map<String, Integer> estoque = new HashMap<>();
+
+        estoque.put("Teclado", 10);
+        estoque.put("Mouse", 20);
+        estoque.put("Celular", 5);
+
+        Collection<Integer> valores = estoque.values();
+
+        int total = 0;
+
+        for (Integer quantidade : valores) {
+            total = total + quantidade;
+        }
+
+        System.out.println("Quantidade total: " + total);
+    }
+}
