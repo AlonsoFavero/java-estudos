518fe6404e215bee46bdb2a81b1c8129e216002a
AlonsoFavero <alonso.favero@gmail.com>
2026-09-15 19:05:06 -0300

    feat: implementa filtro de produtos por preco

---
+++ src/collections/mapas/TesteFiltrarProdutos.java
@@ -0,0 +1,25 @@
+package collections.mapas;
+
+import collections.Listas.Produto;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class TesteFiltrarProdutos {
+    public static void main(String[] args){
+
+        Map<String, Integer> produtos = new HashMap<>();
+
+        produtos.put("Teclado", 200);
+        produtos.put("Mouse", 40);
+        produtos.put("Celular" ,2000);
+
+        for(Entry<String , Integer> meusProdutos: produtos.entrySet()){
+
+            if( meusProdutos.getValue() > 100){
+               System.out.println(meusProdutos);
+            }
+        }
+    }
+}
