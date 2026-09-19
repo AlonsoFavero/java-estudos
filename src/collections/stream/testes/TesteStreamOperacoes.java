9c251fd32a23e5c7cac86c40f8bb1878c1a931fb
AlonsoFavero <alonso.favero@gmail.com>
2026-09-19 17:54:40 -0300

    feat: implementa operacao terminal findFirst com Optional na Stream API

--- src/collections/stream/testes/TesteStreamOperacoes.java
+++ src/collections/stream/testes/TesteStreamOperacoes.java
@@ -1,6 +1,7 @@
 package collections.stream.testes;
 
 import java.util.List;
+import java.util.Optional;
 
 public class TesteStreamOperacoes {
     public static void main(String[] args){
@@ -8,6 +9,7 @@ public class TesteStreamOperacoes {
         List<Integer> numeros = List.of(10,3,5,8,2,7,4);
         List<Integer> todosMaioresQueZero = List.of(10,3,5,8,2,7,4);
         List<Integer> semNegativos = List.of(10,3,5,8,2,7,4);
+        List<Integer> numeros2 = List.of(10,3,5,8,2,7,4);
 
         boolean resultados1 = semNegativos.stream()
                 .noneMatch(n -> n < 0);
@@ -18,8 +20,14 @@ public class TesteStreamOperacoes {
         boolean resultado = numeros.stream()
                 .anyMatch(n -> n == 8);
 
+        Optional<Integer> resultado2 = numeros.stream()
+                .filter(n -> n > 5)
+                .findFirst();
+
+
         System.out.println(resultado);
         System.out.println(resultados);
         System.out.println(resultados1);
+        System.out.println(resultado2);
     }
 }
