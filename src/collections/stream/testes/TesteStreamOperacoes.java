59ba467074982eac44fad0725c0cd23cbb2ef482
AlonsoFavero <alonso.favero@gmail.com>
2026-09-19 17:31:21 -0300

    feat: implementa operacao terminal allMatch na Stream API

--- src/collections/stream/testes/TesteStreamOperacoes.java
+++ src/collections/stream/testes/TesteStreamOperacoes.java
@@ -6,10 +6,15 @@ public class TesteStreamOperacoes {
     public static void main(String[] args){
 
         List<Integer> numeros = List.of(10,3,5,8,2,7,4);
+        List<Integer> todosMaioresQueZero = List.of(10,3,5,8,2,7,4);
+
+        boolean resultados = todosMaioresQueZero.stream()
+                .allMatch(n -> n > 0);
 
         boolean resultado = numeros.stream()
                 .anyMatch(n -> n == 8);
 
         System.out.println(resultado);
+        System.out.println(resultados);
     }
 }
