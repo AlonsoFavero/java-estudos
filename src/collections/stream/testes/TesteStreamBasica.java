ee0a0a29b767522e9798cf03b72c5f3834b695fb
AlonsoFavero <alonso.favero@gmail.com>
2026-09-19 16:51:16 -0300

    feat: adiciona ordenacao alfabetica com sorted na Stream API

--- src/collections/stream/testes/TesteStreamBasica.java
+++ src/collections/stream/testes/TesteStreamBasica.java
@@ -10,7 +10,8 @@ public class TesteStreamBasica {
         // 2. Usando a Stream para filtrar e transformar
         List<String> resultado = cidades.stream()
                 .filter(cidade -> cidade.startsWith("S")) // Pega só as que começam com 'S'
-                .map(String::toUpperCase)                // Transforma em letras maiúsculas
+                .map(String::toUpperCase)
+                .sorted()
                 .toList();                               // Devolve o resultado em uma nova lista
 
         // 3. Mostra o resultado na tela
