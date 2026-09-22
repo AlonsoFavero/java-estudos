4ed9b88b515bfd6d2683de761c677819a9d9ec34
AlonsoFavero <alonso.favero@gmail.com>
2026-09-22 13:42:10 -0300

    feat: implementa tratamento com try catch

---
+++ src/collections/execoes/testes/TesteTryCacth.java
@@ -0,0 +1,15 @@
+package collections.execoes.testes;
+
+public class TesteTryCacth {
+    public static void main(String[] args){
+
+        try{
+
+            int resultado = 10 / 0;
+
+        }catch(ArithmeticException e){
+
+            System.out.println(e);
+        }
+    }
+}
