0a34f34e275d0125815fab08acf4ee1907fccf0f
AlonsoFavero <alonso.favero@gmail.com>
2026-09-22 12:23:34 -0300

    feat: demonstra operações básicas de Optional

--- src/collections/optional/testes/TesteOptional.java
+++ src/collections/optional/testes/TesteOptional.java
@@ -6,7 +6,12 @@ public class TesteOptional {
     public static void main(String[] args){
 
         Optional<String> produtos = Optional.of("valor");
+        Optional<String> produto = Optional.empty();
 
+        System.out.println(produtos);
+        System.out.println(produtos.isPresent());
+        System.out.println(produto.orElse("valor padrão"));
+        System.out.println(produto);
         System.out.println(produtos);
     }
 }
