be6bd4d5856f3bf25979927bf931c8df5692cfe3
AlonsoFavero <alonso.favero@gmail.com>
2026-09-22 12:12:03 -0300

    feat: implementa exercício de Optional

---
+++ src/collections/optional/testes/TesteOptional.java
@@ -0,0 +1,12 @@
+package collections.optional.testes;
+
+import java.util.Optional;
+
+public class TesteOptional {
+    public static void main(String[] args){
+
+        Optional<String> produtos = Optional.of("valor");
+
+        System.out.println(produtos);
+    }
+}
